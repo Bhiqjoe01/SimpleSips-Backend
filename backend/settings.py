d3fa76d63c620577fd1db4d93cf6387d6138d22f
Bhiqjoe01 <fomnyaachari@gmail.com>
2025-12-07 14:56:31 +0100

    home page 3

--- backend/settings.py
+++ backend/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = 'static/'  
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
